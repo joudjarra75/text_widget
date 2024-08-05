e3f1ef23ea0fa13779713ad6e33514dd967304fe
esraaaatmeh <joudjarrah175@gmail.com>
2024-08-05 16:08:21 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,39 @@ class Textwidget extends StatelessWidget{
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: Container
+        body: Column(children: [
+        Container
           (child:Text("Welcome to flutter  Welcome to flutter Welcome to flutter Welcome to flutter Welcome to flutter Welcome to flutter ",
           textDirection:TextDirection.rtl,
           style: TextStyle(fontSize:50,
               backgroundColor:Colors.deepPurpleAccent),
           maxLines: 2,
           overflow:TextOverflow.fade,
-
         ),),
+        Container(
+          child: RichText(text:TextSpan(
+            children: <TextSpan>[
+              TextSpan(text:"W",
+              style: TextStyle(color:Colors.white
+                  ,backgroundColor:Colors.red,
+                  fontSize: 30)),
+              TextSpan(text: "elcome",
+                  style: TextStyle(color:Colors.black,
+                      fontSize: 30)),
+              TextSpan(text: "  To  ",
+                  style: TextStyle(fontWeight:FontWeight.bold,
+                      color:Colors.black,fontSize: 40)),
+              TextSpan(text: "Flutter",
+                  style:TextStyle(color: Colors.black,
+                      decoration:TextDecoration.underline,fontSize:30) ,)
+            ]
+          )),
+        )],
       ),
+        backgroundColor: Colors.blueGrey,//c
+
+      ), //s
+
 
     );
   }
